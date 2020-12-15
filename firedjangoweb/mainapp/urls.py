64ba976aa072e4b5e4e65b4558ee211f5224060a
dev-yujin <kyouj96@gmail.com>
2020-12-15 10:43:36 +0900

    link 화재감지영상보기

--- firedjangoweb/mainapp/urls.py
+++ firedjangoweb/mainapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='main'),
+    path('video/', views.video, name='video'),
 ]
--- firedjangoweb/mainapp/views.py
+++ firedjangoweb/mainapp/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from urllib import parse
+import boto3
 
 def index(request):
+    return render(request, 'mainapp/index.html')
+
+def video(request):
     # AWS_REGION = 'us-east-1'
     AWS_STORAGE_BUCKET_NAME = 'fire-video-s3'
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-    DATETIME = parse.quote('2020-12-09 00:00:00') # url 형식으로 바꾸어 주기
-    FILENAME = '2020-11-10-529c48bf-2312-11eb-be8f-d05099609825.gif'
+    DATETIME = parse.quote('2020-12-09') # url 형식으로 바꾸어 주기
+    FILENAME = '00-00-00.gif'
     VIDEO_URL = 'https://%s/%s/%s' % (AWS_S3_CUSTOM_DOMAIN, DATETIME, FILENAME)
     content = {'static_url': VIDEO_URL }
-    return render(request, 'mainapp/index.html', content)
+    return render(request, 'mainapp/video-view.html', content)
